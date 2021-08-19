bcd913eefe1122f842adc14013474b2f687fb070
Sahil Yadav <sahil.yadav@hcl.com>
2021-08-19 15:27:43 -0400

    First Commit

---
+++ src/main/java/com/hcl/patient/SpringBootPatientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPatientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPatientProjectApplication.class, args);
+	}
+
+}
