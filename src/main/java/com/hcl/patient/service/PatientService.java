71de66b3974b30af1670be88aec983f2d47b7ce3
sahilyadavhcl <82895408+sahilyadavhcl@users.noreply.github.com>
2021-08-20 15:40:10 -0400

    Making model (#6)
    * added file structure
    
    * added models and 1 repo and 1 service started
    
    * fix
    
    * fix
    
    * fix
    
    * Added some service methods and repo
    
    Co-authored-by: sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>

--- src/main/java/com/hcl/patient/service/PatientService.java
+++ src/main/java/com/hcl/patient/service/PatientService.java
@@ -1,5 +1,8 @@
 package com.hcl.patient.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +10,15 @@ import com.hcl.patient.model.Address;
 import com.hcl.patient.model.Patient;
 import com.hcl.patient.model.request.PatientPost;
 import com.hcl.patient.repository.PatientRepository;
-
+/*Create,Read,Update,Delete*/
 @Service
 public class PatientService {
-	@Autowired
 	private PatientRepository pr;
-
+	@Autowired
+	public PatientService(PatientRepository pr){
+		this.pr = pr;
+	}
+	//Create
 	public Patient createPatient(PatientPost pp) {
 		// String street, String city, String state, String landmark, Integer zip
 		Address add = new Address("100 Spring Peak Ct", "Holly Springs", "NC", "By the tower", 27540);
@@ -20,12 +26,24 @@ public class PatientService {
 		// nationalId) {
 		Patient p = new Patient();
 		p.setName(pp.getName());
+		System.out.println(pp.getAddress());
 		p.setAddress(pp.getAddress());
 		p.setPhoneNumber(pp.getPhoneNumber());
 		p.setDob(pp.getDob());
 		p.setNationalId(pp.getNationalId());
-
 		return pr.save(p);
-
 	}
+	//Read by id
+	public Optional<Patient> getPatientById(Long id) {
+		return pr.findById(id);
+	}
+	//ReadAll
+	public List<Patient> getAllPatient(){
+		return pr.findAll();
+	}
+	//Delete
+	public void deletePatient(Long id) {
+		pr.deleteById(id);
+	}
+	
 }
